0c26cf1cfeeefee646d50162462fd77ef68e0ce8
sgomeza13 <sgomeza13@eafit.edu.co>
2023-11-24 17:49:53 -0500

    testing partitionkey as videoId

--- GetChat.py
+++ GetChat.py
@@ -8,15 +8,15 @@ def getLiveData(videoId):
     chat = pytchat.create(video_id=videoId)
     while chat.is_alive():
         for c in chat.get().sync_items():
-            print(f"Video:{videoId} {c.datetime} [{c.author.name}]- {c.message}")
-            '''
+            #print(f"Video:{videoId} {c.datetime} [{c.author.name}]- {c.message}")
+            
             data = {'videoId':videoId,
                     'datetime': c.datetime,
                    'author': c.author.name,
                    'message': c.message}
             response = kinesis_client.put_record(StreamName='LiveYTChat',
                                                  Data=str(data),
-                                                 PartitionKey='partition_key')
+                                                 PartitionKey=videoId)
             
             print(f"Record sent to Kinesis with sequence number: {response['SequenceNumber']}")
-            '''
+            
